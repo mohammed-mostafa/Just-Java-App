c92a85a9dbfed4d2fb5164f267eb6ee8c7e1e0d2
mostafa <mohammed.mostufa@yahoo.com>
2018-03-06 20:47:50 +0200

    I don't know

--- app/src/main/java/com/example/andriod/justjava/MainActivity.java
+++ app/src/main/java/com/example/andriod/justjava/MainActivity.java
@@ -32,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
 
-        String message = " Item count " + quantity ;
-        displayMessage(message);
+        displayPrice(quantity * 5);
     }
     /**
      * This method is called when the order + is clicked.
